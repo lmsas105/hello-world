02640b785824de669d9dbea43bcde30874e516b2
lmsas105 <lmsas105.github.io>
2022-02-09 20:22:04 +0900

    인사 추가

--- src/Helloworld.java
+++ src/Helloworld.java
@@ -3,5 +3,6 @@ public class Helloworld {
 	public static void main(String[] args) {
 		System.out.println("Hi World");
 		System.out.println("Hello World");
+		System.out.println("Hellohi World");
 	}
 }
